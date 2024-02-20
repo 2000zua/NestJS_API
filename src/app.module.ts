0691b0cb17a597867ee751cf58463ed76f8812d6
Angelo Abreu Zua <Gildozua1@hotmail.com>
2024-02-21 00:55:17 +0300

    sd

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BoolmarkModule } from './boolmark/boolmark.module';
+
+@Module({
+  imports: [
+    AuthModule,
+    UserModule,
+    BoolmarkModule
+  ]
+})
+export class AppModule {}
